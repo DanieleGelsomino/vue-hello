c4106eedb8ce84d3da1bbc4a7087eb61efe15091
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-24 14:00:33 +0100

    add message add image add class container complete

--- vuejs/vue.js
+++ vuejs/vue.js
@@ -1,5 +1,18 @@
 console.log("VUE JS OK!");
 
+/*
+    Descrizione:
+    Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+    Bonus:
+    Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
 const app = new Vue({
-  el: "#vue-app",
+  el: "#root",
+  data: {
+    message: "Zlatan Ibrahimovic",
+    miaclasse: "container",
+    image:
+      "https://w0.peakpx.com/wallpaper/765/392/HD-wallpaper-zlatan-ibrahimovic-11-ac-milan-acmilan-ibra-iz-back-milan-rossoneri.jpg",
+  },
 });
